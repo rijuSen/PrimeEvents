48556b6c05f5a9d7d58a27997b230de53b1fba7a
Rijuman Sen <rsen0005@student.monash.edu>
2019-10-12 18:33:34 +1100

    main reformatting

--- main.py
+++ main.py
@@ -1,73 +1,77 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-import time
 import getpass
 import os
+import time
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+
 from user.user import *
-from hall.hall import *
-from controller import Session
+
+from customerController import customerController
 
 def navOptions(selection, state):
     if selection == 'O':
         state = 1
     elif selection == 'B':
-        state = state - 1 
+        state = state - 1
     elif selection == 'E':
         state = 0
     return state
 
+
 def displayPage(pageName, userName, optionDisplay, pageNavDict):
     os.system('clear')
-    #Display Page Name
-    print('-'*45)
+    # Display Page Name
+    print('-' * 45)
     print('{0:^45}'.format(pageName))
-    print('-'*45)
-    #Display User Name
+    print('-' * 45)
+    # Display User Name
     if not len(userName) == 0:
-        print('{0:^45}'.format('Logged in as '+userName.capitalize()))
-        print('-'*45)
+        print('{0:^45}'.format('Logged in as ' + userName.capitalize()))
+        print('-' * 45)
     if not len(optionDisplay) == 0:
-        if isinstance(optionDisplay,dict):
-            #Menu Options format
+        if isinstance(optionDisplay, dict):
+            # Menu Options format
             print('{:^45}'.format('Input key to select corresponding option'))
-            print('-'*45)
-            print('{0:^10}{1:^30}'.format('[Keys]','Options'))
-            print('-'*45)
-            #display menu
+            print('-' * 45)
+            print('{0:^10}{1:^30}'.format('[Keys]', 'Options'))
+            print('-' * 45)
+            # display menu
             for key, option in optionDisplay.items():
                 print('{0:>4}{1}{2:<5}{3:^30}'.format('[', key, ']', option))
-            print('-'*45)
-            #navigation panel
-            print('-'*45)
-        if isinstance(optionDisplay,list):
-            #Menu Options format
+            print('-' * 45)
+            # navigation panel
+            print('-' * 45)
+        if isinstance(optionDisplay, list):
+            # Menu Options format
             print('Input key to select corresponding option')
-            print('-'*45)
-            #display menu
-            tableHeader = ("{0:^5}{1:^10}{2:^10}{3:^10}{4:^10}".format('Key','Venue','Type','Addr','Capacity'))
+            print('-' * 45)
+            # display menu
+            tableHeader = ("{0:^5}{1:^10}{2:^10}{3:^10}{4:^10}".format('Key', 'Venue', 'Type', 'Addr', 'Capacity'))
             print("{0:^45}".format(tableHeader))
             for row in optionDisplay:
-                rowWise = ("{0:^5}{1:^10}{2:^10}{3:^10}{4:^10}".format(row[0],row[1],row[3],row[4],row[5]))
+                rowWise = ("{0:^5}{1:^10}{2:^10}{3:^10}{4:^10}".format(row[0], row[1], row[3], row[4], row[5]))
                 print('{:^45}'.format(rowWise))
-            print('-'*45)
-            #navigation panel
-            print('-'*45)
+            print('-' * 45)
+            # navigation panel
+            print('-' * 45)
     if not len(pageNavDict) == 0:
         navBar = ''
         for key, option in pageNavDict.items():
-            navBarTemp = '{:^11}'.format('['+key+']'+option)
+            navBarTemp = '{:^11}'.format('[' + key + ']' + option)
             navBar = navBar + navBarTemp
         print('{:^45}'.format(navBar))
-    print('-'*45)
+    print('-' * 45)
 
-def selectOption(optionDisplay,pageNavDict):
-    #prompt user to select option
+
+def selectOption(optionDisplay, pageNavDict):
+    # prompt user to select option
     selection = input('Enter your selection: ')
     if isinstance(optionDisplay, dict) and selection in optionDisplay.keys():
         print('Your selection: {}'.format(optionDisplay.get(selection)))
         return False, selection
     elif isinstance(optionDisplay, list) and selection.isdigit() and int(selection) <= len(optionDisplay):
-        print('Your selection: {}'.format(optionDisplay[int(selection)-1]))
+        print('Your selection: {}'.format(optionDisplay[int(selection) - 1]))
         return False, selection
     elif selection in pageNavDict.keys():
         print('Your selection: {}'.format(pageNavDict.get(selection)))
@@ -79,12 +83,12 @@ def selectOption(optionDisplay,pageNavDict):
 
 
 def getPass():
-    #check password length more than or equal to 8
+    # check password length more than or equal to 8
     passFlag = False
     while not passFlag:
         passPlain = getpass.getpass(prompt='Enter Password(must be >= 8): ')
         if len(passPlain) >= 8:
-            passFlag = True 
+            passFlag = True
         else:
             passFlag = False
             print("Password must have 8 or more characters")
@@ -93,11 +97,12 @@ def getPass():
     passHash = digest.finalize()
     return passHash
 
+
 def acceptUserDetails():
     os.system('clear')
-    print('='*41)
+    print('=' * 41)
     print('{:^41}'.format('Registration Page'))
-    print('='*41)
+    print('=' * 41)
     userInfo = dict()
     userInfo['firstName'] = input('Enter First Name: ')
     userInfo['lastName'] = input('Enter Last Name: ')
@@ -113,8 +118,9 @@ def acceptUserDetails():
             print('Maximum attemps reached, taking back to login page')
             time.sleep(2)
         else:
-            userInfo['password'] = getPass() 
-    return mailExistFlag, userInfo 
+            userInfo['password'] = getPass()
+    return mailExistFlag, userInfo
+
 
 def userLogin():
     loginFlag = False
@@ -136,63 +142,58 @@ def userLogin():
             success = False
         else:
             success = True
-    return {'success':success,'userObj':userObj}
+    return {'success': success, 'userObj': userObj}
 
 
 def displayTableFormat(listData, startIndex):
-    print("{0:^5}{1:^10}{2:^10}{3:^10}{4:^10}".format('ID','Hall-Name','Hall-Type','Hall-Addr','Hall-Capacity'))
+    print("{0:^5}{1:^10}{2:^10}{3:^10}{4:^10}".format('ID', 'Hall-Name', 'Hall-Type', 'Hall-Addr', 'Hall-Capacity'))
     for row in listData[startIndex:startIndex + 4:1]:
-        print("{0:^5}{1:^10}{2:^10}{3:^10}{4:^10}".format(row[0],row[1],row[3],row[4],row[5]))
+        print("{0:^5}{1:^10}{2:^10}{3:^10}{4:^10}".format(row[0], row[1], row[3], row[4], row[5]))
 
 
 def main():
-    adminPage = {'1':'Manager Users/Owners','2':'Hall Listing','3':'Manage Discounts'}
-    registerPage = {'F':'First Name', 'L': 'Last Name', 'E': 'Email', 'P': 'Password'}
-    ownerPage = {'1':'Manager Halls','2':'Manage Bookings','3':'View Quotation Request','4':'Manage Payments','5':'Manage Discounts'}
     state = 1
-    onFlag = True
-    print(onFlag)
     while state > 0:
-        #state 1 represent login action
-        while state == 1: 
+        # state 1 represent login action
+        while state == 1:
             os.system('clear')
             landingPage = {'L': 'Login', 'O': 'Register as Owner', 'C': 'Register as Customer'}
-            navPageDict = {'E' : 'Exit'}
+            navPageDict = {'E': 'Exit'}
             userNamePlaceHolder = ''
             displayPage('Login Page', userNamePlaceHolder, landingPage, navPageDict)
             invalidSelectionFlag, selection = selectOption(landingPage, navPageDict)
             if not invalidSelectionFlag:
                 if selection == 'O':
                     mailExistFlag, userInfo = acceptUserDetails()
-                    #create a user object
+                    # create a user object
                     if mailExistFlag:
                         state = 1
                     else:
                         userObj = Owner(userInfo)
-                        print('User info is {}'.format(userInfo))
-                        time.sleep(2)
-                        sessionObj = Session(userObj)
-                        #print(sessionObj.getSessionId())
                         state = 2
-                        print('State is {} and session ID is {} and user type is {}'.format(state,sessionObj.getSessionId(),sessionObj.getUserType()))
-                        #print(owner.getRowId())
+                        print('User info is {}'.format(userInfo))
+                        print('State is {} and session ID is {} and user type is {}'.format(state,
+                                                                                            userObj.getRowId(),
+                                                                                            userObj.getUserType()))
+                        time.sleep(2)
+                        # print(owner.getRowId())
                 elif selection == 'C':
                     mailExistFlag, userInfo = acceptUserDetails()
-                    #create a user object
+                    # create a user object
                     if mailExistFlag:
                         state = 1
                     else:
                         userObj = Customer(userInfo)
-                        print('User info is {}'.format(userInfo))
-                        time.sleep(2)
-                        #print(sessionObj.getSessionId())
                         state = 2
-                        #print(customer.getRowId())
+                        print('User info is {}'.format(userInfo))
+                        print('State is {} and session ID is {} and user type is {}'.format(state,
+                                                                                            userObj.getRowId(),
+                                                                                            userObj.getUserType()))
+                        time.sleep(2)
                 elif selection == 'L':
                     objDict = userLogin()
                     if objDict['success']:
                         if objDict['userObj'].getAllowFlag() == 0:
-                            sessionObj = Session(objDict['userObj'])
                             state = 2
                             userObj = objDict['userObj']
                         elif objDict['userObj'].getAllowFlag() == 1:
@@ -203,135 +204,15 @@ def main():
                     exit()
             else:
                 print('Invalid selection, Please input again')
-        print('State is {} and session ID is {} and user type is {}'.format(state,sessionObj.getSessionId(),sessionObj.getUserType()))
-        
-        #state 2 represent actions post login
-        while state == 2 and sessionObj.getUserType() == 'Customer':
-            customerPage = {'1':'Search Halls','2':'Manager Bookings'}
-            navPageDict = {'O': 'Logout', 'E': 'Exit'}
-            displayPage('Customer Page', sessionObj.getFirstName(), customerPage, navPageDict)
-            invalidSelectionFlag, selection = selectOption(customerPage, navPageDict)
-            #for navigation menu
-            if not invalidSelectionFlag:
-                if selection in navPageDict:
-                    state = navOptions(selection, state)
-                else:
-                    #take to next state to display hall listing
-                    state = 3
-            else:
-                print('Invalid selection, Please input again')
-        
-        #state 3 is for hall listing for any user
-        while state == 3 and selection == '1':
-#            navPageDict = {'B': 'Go Back', 'N': 'Next Page', 'O': 'Logout', 'E': 'Exit'}
-            navPageDict = {'B': 'Go Back', 'O': 'Logout', 'E': 'Exit'}
-            allEntries = Hall.viewAllHalls()
-            index = 0
-            displayPage('Hall Listing', sessionObj.getFirstName(), allEntries, navPageDict)
-            invalidSelectionFlag, selection = selectOption(allEntries, navPageDict)
-            if not invalidSelectionFlag:
-                if selection in navPageDict:
-                    state = navOptions(selection, state)
-                if selection.isdigit():
-                    if int(selection) < len(allEntries) + 1:
-                        print(allEntries[int(selection) + 1])
-                        exit()
+        print('State is {} and session ID is {} and user type is {}'.format(state, userObj.getRowId(),
+                                                                            userObj.getUserType()))
+        # call respective controller to deal with further action
+        while state == 2 and userObj.getUserType() == 'Customer':
+            customerController(userObj)
+        while state == 2 and userObj.getUserType() == 'Owner':
+            ownerController(userObj)
 
-
-#            while index + 4 < len(allEntries):
-#                displayPage('Hall Listing', sessionObj.getFirstName(), allEntries[index: index + 4] navPageDict)
-#                invalidSelectionFlag, selection = selectOption(customerPage, navPageDict)
-#                if not invalidSelectionFlag:
-#                    if selection == 'B' and index == 0:
-#                        state = navOptions(selection,state)
-#                    elif selection == 'B' and index > 0:
-#                        index = index - 4
-#                        displayPage('Hall Listing', sessionObj.getFirstName(), allEntries[index:index + 4] navPageDict)
-#                    elif selection == 'N'  and index + 4 < len(allEntries):
-#                        displayPage('Hall Listing', sessionObj.getFirstName(), allEntries[index:index + 4] navPageDict)
-#                        index = index + 4
-#                    elif selection == 'N' and index + 4 > len(allEntries):
-#                        displayPage('Hall Listing', sessionObj.getFirstName(), allEntries[index:] navPageDict)
-#                    if selection in navPageDict:
-#                        state = navOptions(selection, state)
-#if selecti print('Yo')
-            #exit()
-        
-        
-#        while state == 3 and selection == '2':
-#            pass
-#
-#
-#
-#                if selection == '1':
-#                    navPageDict = {'B': 'Go Back', 'N': 'Next Page', 'O': 'Logout', 'E': 'Exit'}
-#                    allEntries = Hall.viewAllHalls()
-#                    index = 0
-#                    displayTableFormat(allEntries, index)
-#                    invalidSelectionFlag, selection = selectOption(customerPage, navPageDict)
-#                    while not invalidSelectionFlag:
-#                        if selection == 'B':
-#                            break
-#                        if selection == 'P':
-#                            if index != 0:
-#                                index = index - 4
-#                                displayTableFormat(allEntries, index)
-#                            else:
-#                                pass
-#
-#
-#                elif selection == 'E':
-#                                    exit()
-#            else:
-#                print('Invalid selection, Please input again')
-#            print('State is {} and session ID is {} and user type is {}'.format(state,sessionObj.getSessionId(),sessionObj.getUserType()))
-
-                    #                for startIndex in range(0,len(allEntries),4):
-                     #                   displayTableFormat(allEntries,startIndex)
-                                        
-
-#        while state == 2 and userType == 'Owner':
-#            selection = displayPage('Owner Page', firstName, ownerPage, navPageDict)
-#            state = navOptions(selection, state)
-#
-#        while state == 2 and userType == 'Admin':
-#            selection = displayPage('Admin Page', firstName, adminPage, navPageDict)
-#            state = navOptions(selection, state)
-#
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       #    displayPage('OwnerHomePage',ownerPage)
+    #    displayPage('OwnerHomePage',ownerPage)
     #    manageHallPage = {'0':'Go Back','1':'View Halls','2':'Create Hall'}
     #    displayPage('ManageHallPage',manageHallPage)
     #    createHallPage = {'0':'Go Back','1':'Enter Hall Name: ','2':'Enter Hall Capacity: ','3':'Enter Hall Size: ','4':'Enter Hall Location: '}
@@ -367,6 +248,6 @@ def main():
     #    createDiscountPage = {'0':'Go Back','1':'Enter Hall Name: ','2':'Enter Discount Percentage: '}
     #    displayPage('CreateDiscountPage',createDiscountPage)
 
-if __name__=="__main__":
-    main()
 
+if __name__ == "__main__":
+    main()
--- user/user.py
+++ user/user.py
@@ -1,16 +1,17 @@
-#class for user in prime events
+# class for user in prime events
 import sqlite3
 from pathlib import Path
 
+
 class User:
     '''docstring'''
-    
-    #class variable to define the path to the DB file
+    # class variable to define the path to the DB file
     dbFileName = "databaseFiles/primeEventsDb.db"
-    def insertIntoUserDb(self,firstName,lastName,email,password,userType,allowFlag):
+
+    def insertIntoUserDb(self, firstName, lastName, email, password, userType, allowFlag):
         ''' Function needs arguments --> firstName,lastName,email,password,userType,allowFlag'''
         self.userDbFilePath = Path(User.dbFileName)
-        #check if database file already exists
+        # check if database file already exists
         if not self.userDbFilePath.is_file():
             conn = sqlite3.connect(User.dbFileName)
             c = conn.cursor()
@@ -24,38 +25,41 @@ class User:
                         UNIQUE(email))
                         """)
             conn.close()
-        
-        #insert entry into database
+
+        # insert entry into database
         try:
             conn = sqlite3.connect(User.dbFileName)
             c = conn.cursor()
             with conn:
                 c.execute("INSERT INTO users VALUES (:firstName, :lastName, :email, :password, :userType, :allowFlag)",
-                        {'firstName': firstName, 'lastName': lastName, 'email': email, 'password': password, 'userType': userType, 'allowFlag': allowFlag})
-            c.execute("SELECT rowid from users WHERE email = :email",{'email': email,})
-            
-            #save the rowid of the inserted row in the variable rowId
+                          {'firstName': firstName, 'lastName': lastName, 'email': email, 'password': password,
+                           'userType': userType, 'allowFlag': allowFlag})
+            c.execute("SELECT rowid from users WHERE email = :email", {'email': email, })
+
+            # save the rowid of the inserted row in the variable rowId
             for id in c.fetchone():
                 self.rowId = id
-        
+
         except sqlite3.Error as sqlite3Error:
-            print("SQLite3 Error : -->",sqlite3Error)
+            print("SQLite3 Error : -->", sqlite3Error)
         finally:
             conn.close()
-        
-    def checkPassword(self,userInfo):
+
+    def checkPassword(self, userInfo):
         email = userInfo['email']
         password = userInfo['password']
         conn = sqlite3.connect(self.dbFileName)
         c = conn.cursor()
-        c.execute("SELECT rowid, firstName, lastName, email, password, userType, allowFlag FROM users WHERE email = :email AND password = :password",{'email': email, 'password': password})
+        c.execute(
+            "SELECT rowid, firstName, lastName, email, password, userType, allowFlag FROM users WHERE email = :email AND password = :password",
+            {'email': email, 'password': password})
         row = c.fetchone()
         return row
-         
-    #def __init__(self,firstName,lastName,email,password,userType):
-    
-    def __init__(self,userInfo):
-        #create a new entry in the DB
+
+    # def __init__(self,firstName,lastName,email,password,userType):
+
+    def __init__(self, userInfo):
+        # create a new entry in the DB
         if len(userInfo) == 5:
             self.firstName = userInfo['firstName']
             self.lastName = userInfo['lastName']
@@ -63,9 +67,10 @@ class User:
             self.password = userInfo['password']
             self.userType = userInfo['userType']
             self.allowFlag = 0
-            self.insertIntoUserDb(self.firstName,self.lastName,self.email,self.password,self.userType,self.allowFlag)
+            self.insertIntoUserDb(self.firstName, self.lastName, self.email, self.password, self.userType,
+                                  self.allowFlag)
             self.success = True
-        #create an object of an existing entry
+        # create an object of an existing entry
         if len(userInfo) == 2:
             row = self.checkPassword(userInfo)
             if not row == None:
@@ -112,29 +117,32 @@ class User:
         cls.dbFileName = newDbName
 
     @classmethod
-    def editUser(cls,editEntryWithEmail,firstName,lastName,password):
+    def editUser(cls, editEntryWithEmail, firstName, lastName, password):
         """Only first name, last name and password can be modified"""
         conn = sqlite3.connect(self.dbFileName)
         c = conn.cursor()
         try:
             with conn:
-                c.execute("""UPDATE users SET firstName = :fname, lastName = :lname, password = :pass WHERE email = :email""",{'fname': firstName, 'lname': lastName, 'password': password, 'email': editEntryWithEmail})
+                c.execute(
+                    """UPDATE users SET firstName = :fname, lastName = :lname, password = :pass WHERE email = :email""",
+                    {'fname': firstName, 'lname': lastName, 'password': password, 'email': editEntryWithEmail})
         except sqlite3.Error as sqlite3Error:
-            print("SQLite3 Error -->",sqlite3Error)
+            print("SQLite3 Error -->", sqlite3Error)
         finally:
             conn.close()
-    
 
     @classmethod
-    def emailExists(cls,email):
+    def emailExists(cls, email):
         conn = sqlite3.connect(cls.dbFileName)
         c = conn.cursor()
-        c.execute("SELECT count(*) FROM users WHERE email = :email",{'email':email,})
-        data=c.fetchone()[0]
-        if data==0:
+        c.execute("SELECT count(*) FROM users WHERE email = :email", {'email': email, })
+        data = c.fetchone()[0]
+        if data == 0:
             return False
         else:
             return True
+
+
 #    @classmethod
 #    def displayEntry(cls,email):
 #        conn = sqlite3.connect(cls.dbFileName)
@@ -142,23 +150,16 @@ class User:
 #        c.execute("SELECT firstName, lastName, email,  from users WHERE email = :email",{'email': email,})
 #        for column in c.fetchone():
 
-    #take a mail id and password and return a user object
-           
-
-
-
-
-
+# take a mail id and password and return a user object
 
 
 class Owner(User):
     '''Owner class extending User'''
 
-    def __init__(self,userInfo):
-        userInfo['userType'] = "Owner"            
+    def __init__(self, userInfo):
+        userInfo['userType'] = "Owner"
         super().__init__(userInfo)
 
-    
     def getOwnerHalls(userId):
         pass
 
@@ -172,8 +173,8 @@ class Owner(User):
 class Customer(User):
     '''Customer class extending User'''
 
-    def __init__(self,userInfo):
-        userInfo['userType'] = "Customer"            
+    def __init__(self, userInfo):
+        userInfo['userType'] = "Customer"
         super().__init__(userInfo)
 
     def getCustomerBookingHistory(userId):
@@ -183,13 +184,12 @@ class Customer(User):
         pass
 
 
-
 class Admin(User):
     '''Admin class extending User'''
-    
-    def __init__(self,firstName,lastName,email,password):
-        self.userType = "Admin"            
-        super().__init__(firstName,lastName,email,password,self.userType)
+
+    def __init__(self, firstName, lastName, email, password):
+        self.userType = "Admin"
+        super().__init__(firstName, lastName, email, password, self.userType)
 
     def viewAllUsers():
         conn = sqlite3.connect(User.dbFileName)
@@ -197,7 +197,7 @@ class Admin(User):
         c.execute("SELECT rowid,firstName, lastName, email, userType, allowFlag FROM users")
         output = c.fetchall()
         for entry in output:
-            print(entry,end='\n')
+            print(entry, end='\n')
         conn.close()
 
     def blockUser(rowid):
@@ -205,17 +205,18 @@ class Admin(User):
         c = conn.cursor()
         try:
             with conn:
-                #c.execute("SELECT last_insert_rowid()")
-                #print(c.fetchall())
-                c.execute("""UPDATE users SET allowFlag = :allowFlag WHERE rowid = :rowid""",{'allowFlag': 1, 'rowid':rowid})
-                #print(c.fetchone())
-                #if 'None' in str(c.fetchone()):
-                 #   print('Id:',rowid,'is not present in the database')
-               # else:
-                #    c.execute("SELECT rowid,* FROM users WHERE rowid = :rowid",{'rowid': rowid,})
-                 #   print(c.fetchone())
+                # c.execute("SELECT last_insert_rowid()")
+                # print(c.fetchall())
+                c.execute("""UPDATE users SET allowFlag = :allowFlag WHERE rowid = :rowid""",
+                          {'allowFlag': 1, 'rowid': rowid})
+                # print(c.fetchone())
+                # if 'None' in str(c.fetchone()):
+                #   print('Id:',rowid,'is not present in the database')
+            # else:
+            #    c.execute("SELECT rowid,* FROM users WHERE rowid = :rowid",{'rowid': rowid,})
+            #   print(c.fetchone())
         except sqlite3.Error as sqlite3Error:
-            print("SQLite3 Error -->",sqlite3Error)
+            print("SQLite3 Error -->", sqlite3Error)
         finally:
             conn.close()
 
